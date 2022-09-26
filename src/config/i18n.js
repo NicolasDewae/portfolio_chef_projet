c42d2d35bcfc59413e82c6d9c0b4382a65aa5b5c
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-09-26 22:19:18 +0200

    i18n file added

---
+++ src/config/i18n.js
@@ -0,0 +1,165 @@
+export const servicesListData = [
+    {
+        fr: [
+                {
+                    id: 1,
+                    title: 'Développement Web',
+                    description: 'Je peux créer une application web magnifique et scalable en utilisant HTML, CSS et React.js',
+                },
+                {
+                    id: 2,
+                    title: 'Développement d\'application mobile',
+                    description: 'Je peux créer une application mobile magnifique et scalable en utilisant React Native',
+                },
+                {
+                    id: 3,
+                    title: 'Développement de logiciel',
+                    description: 'Je peux créer un logiciel en utilisant C++',
+                },
+                {
+                    id: 4,
+                    title: 'UI/UX Design',
+                    description: 'Je peux créer une interface utilisateur magnifique et scalable en utilisant Figma',
+                }
+        ],
+        en: [
+            {
+                id: 1,
+                title: 'Web Development',
+                description: 'I can build a beautiful and scalable SPA using HTML, CSS and React.js',
+            },
+            {
+                id: 2,
+                title: 'Mobile App Development',
+                description: 'I can build a beautiful and scalable mobile app using React Native',
+            },
+            {
+                id: 3,
+                title: 'Software Development',
+                description: 'I can develop a software using C++',
+            },
+            {
+                id: 4,
+                title: 'UI/UX Design',
+                description: 'I can design a beautiful and scalable UI/UX using Figma',
+            }
+        ]
+    }
+
+]
+
+export const about = [
+    {
+        fr: [
+                {
+                    title: 'À propos de moi',
+                    description: 'Je suis un développeur web et un développeur de logiciel. J\'ai de l\'expérience dans la création d\'applications web en utilisant React.js et Node.js. J\'ai aussi de l\'expérience dans la création d\'applications mobile en utilisant React Native. J\'ai de l\'expérience dans le développement de logiciel en utilisant C++.',
+                }
+        ],
+        en: [
+            {
+                title: 'About Me',
+                description: 'I am a full stack web developer and a software developer. I have experience in building web applications using React.js and Node.js. I have also experience in building mobile applications using React Native. I have experience in developing software using C++.',
+            }
+        ],
+
+    }
+]
+
+export const contact = [
+    {
+        fr: [
+            {
+                title: 'Contactez moi',
+                description: '',
+                image: '',
+                phone: '06 00 00 00 00',
+                email: 'monEmail@mail.fr',
+                address: '1 rue de Paris',
+                social: {
+                    facebook: '',
+                    twitter: '',
+                    instagram: '',
+                    linkedin: '',
+                    github: '',
+                }     
+            }
+        ],
+        en: [
+                {
+                    title: 'Contact Me',
+                    description: 'I am available for freelance work. If you have a project that you want to get started, think you need my help with something or just fancy saying hello, then get in touch.',
+                    image: '',
+                    phone: '06 00 00 00 00',
+                    email: 'monEmail@mail.fr',
+                    address: '1 rue de Paris',
+                    social: {
+                        facebook: '',
+                        twitter: '',
+                        instagram: '',
+                        linkedin: '',
+                        github: '',
+                    }
+                }
+        ]
+    }
+]
+
+export const navLink = [
+    {
+        fr: [
+                {
+                    id: 1,
+                    title: 'Accueil',
+                    url: '#',
+                },
+                {
+                    id: 2,
+                    title: 'À propos',
+                    url: '#about',
+                },
+                {
+                    id: 3,
+                    title: 'Services',
+                    url: '#services',
+                },
+                {
+                    id: 4,
+                    title: 'Mes travaux',
+                    url: '#travaux',
+                },
+                {
+                    id: 5,
+                    title: 'Contact',
+                    url: '#contact',
+                }
+        ],
+        en: [
+            {
+                id: 1,
+                title: 'Home',
+                url: '#',
+            },
+            {
+                id: 2,
+                title: 'About',
+                url: '#about',
+            },
+            {
+                id: 3,
+                title: 'Services',
+                url: '#services',
+            },
+            {
+                id: 4,
+                title: 'Works',
+                url: '#works',
+            },
+            {
+                id: 5,
+                title: 'Contact',
+                url: '#contact',
+            }
+    ],
+    }
+]
