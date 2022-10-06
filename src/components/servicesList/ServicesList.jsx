3ae0d3b376aebbf8a5dc379ccca5dfc8e388c97a
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-10-06 13:36:06 +0200

    liste updated

--- src/components/servicesList/ServicesList.jsx
+++ src/components/servicesList/ServicesList.jsx
@@ -5,24 +5,28 @@ import { servicesListData } from '../../config/i18n';
 const ServiceList = ( translate ) => {
     return (
         <>
-            <div className="service">
-                <div className="service_content">
-                    <div className="service_list">
-                        {servicesListData.map((serviceData) => {
-                            serviceData = translate.data ? serviceData.fr : serviceData.en;
-                            return (
-                                serviceData.map((item) => {
-                                    return (
-                                        
-                                        <div className="service_item">
-                                            <h2>{item.title}</h2>
-                                            <p>{item.description}</p>
-                                        </div>
-                                    )
-                                })
-                            )
-                        })}
-                    </div>
+            <div id="service">
+                <div className="service_list">
+                    {servicesListData.map((serviceData) => {
+                        serviceData = translate.data ? serviceData.fr : serviceData.en;
+                        return (
+                            serviceData.map((item) => {
+                                return (
+                                    
+                                    <div className="service_item">
+                                        <h1>{item.title}</h1>
+                                        {Object.values(item.description).map((description, index) => {
+                                            return (
+                                                <div key={index} className="timeline_item_content_description">
+                                                    <p>{description}</p>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                )
+                            })
+                        )
+                    })}
                 </div>
             </div>
         </>
