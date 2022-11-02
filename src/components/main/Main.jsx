c2023d7fbfc37ac7c04efaac11cf0d3a202fc19b
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-11-02 09:26:41 +0100

    name data added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Main.css';
+import { userInfos } from '../../config/i18n';
 
 const Main = () => {
     return (
         <>
             <div id="main">
                 <div className="main_content">
-                    <h1>Prenom Nom</h1>
+                    <h1>{userInfos.firstName + ' ' +userInfos.lastName}</h1>
                     </div>
             </div>
         </>
